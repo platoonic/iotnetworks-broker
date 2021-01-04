6519b27e6d4921e95a767a620bbc191d6efb89e8
Khalid Khalil <coderizer@aol.com>
2021-01-04 16:50:30 +0200

    Initialize MQTT broker

---
+++ index.js
@@ -0,0 +1,12 @@
+const aedes = require("aedes")();
+const httpServer = require("http").createServer();
+const ws = require("websocket-stream");
+
+ws.createServer({ server: httpServer }, aedes.handle);
+
+httpServer.listen(process.env.PORT, function () {
+  console.log(
+    "websocket server listening on process.env.PORT ",
+    process.env.PORT
+  );
+});
