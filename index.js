111d4abd7e4af33d0fdc83fc72c0cb86bf8c0bf9
Khalid Khalil <coderizer@aol.com>
2021-01-05 20:05:17 +0200

    Separate direct MQTT and WS servers

--- index.js
+++ index.js
@@ -1,12 +1,47 @@
+"use strict";
+
 const aedes = require("aedes")();
+const server = require("net").createServer(aedes.handle);
 const httpServer = require("http").createServer();
 const ws = require("websocket-stream");
+const port = 1883;
+const wsPort = 8888;
 
-ws.createServer({ server: httpServer }, aedes.handle);
+server.listen(port, function () {
+  console.log("MQTT listening on port", port);
+});
 
-httpServer.listen(process.env.PORT, function () {
-  console.log(
-    "websocket server listening on process.env.PORT ",
-    process.env.PORT || 3000
-  );
+ws.createServer(
+  {
+    server: httpServer,
+  },
+  aedes.handle
+);
+
+httpServer.listen(wsPort, function () {
+  console.log("websocket server listening on port", wsPort);
+});
+
+aedes.on("clientError", function (client, err) {
+  console.log("client error", client.id, err.message, err.stack);
+});
+
+aedes.on("connectionError", function (client, err) {
+  console.log("client error", client, err.message, err.stack);
+});
+
+aedes.on("publish", function (packet, client) {
+  if (client) {
+    console.log("message from client", client.id);
+  }
+});
+
+aedes.on("subscribe", function (subscriptions, client) {
+  if (client) {
+    console.log("subscribe from client", subscriptions, client.id);
+  }
+});
+
+aedes.on("client", function (client) {
+  console.log("new client", client.id);
 });
